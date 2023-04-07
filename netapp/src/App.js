5160a42a22b4378a2ab4792cb31adb3f38a8bcbc
Nitya-Pasrija <nitya055btit21@igdtuw.ac.in>
2023-04-07 19:22:03 +0530

    next

--- netapp/src/App.js
+++ netapp/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Home from './pages/Home';
+import Signup from './pages/Signup';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home/>}/>
+          <Route path='/signup' element={<Signup/>}/>
         </Routes>
     </BrowserRouter>
   );

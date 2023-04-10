ce16fdf23cde039e33e6d1cac2924c3ed6700d28
Akanksha Jha <100708686+Akanksha-202@users.noreply.github.com>
2023-04-10 20:18:13 +0530

    Add files via upload

---
+++ netapp/src/pages/Profile.jsx
@@ -0,0 +1,181 @@
+import React from 'react';
+
+function ProfilePage() {
+    return (
+        <div className="bg-gray-100 min-h-screen">
+            <header className="bg-white shadow-lg">
+                <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div className="flex justify-between h-16">
+                        <div className="flex">
+                            <div className="flex-shrink-0 flex items-center">
+                                <img
+                                    className="h-8 w-auto"
+                                    src="https://tailwindui.com/img/logos/workflow-mark-on-white.svg"
+                                    alt="Workflow"
+                                />
+                            </div>
+                        </div>
+                        <div className="hidden sm:ml-6 sm:flex sm:items-center">
+                            <a href="#" className="px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Dashboard</a>
+                            <a href="#" className="px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"> Team </a>
+                            <a href="#" className="px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"> Projects</a>
+                            <a href="#" className="px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"> Calendar</a>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+            <main className="py-10">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="overflow-hidden sm:rounded-lg">
+                        <img
+                            className="h-48 w-full object-cover"
+                            src="https://images.unsplash.com/photo-1611869567082-017165ae7cc7"
+                            alt="Profile banner"
+                        />
+                    </div>
+                    <div className="mt-8 max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
+                        <div className="flex items-center justify-between">
+                            <div className="flex-1 min-w-0">
+                                <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">John Doe</h2>
+                                <div className="mt-1 flex flex-col sm:flex-row sm:flex-wrap sm:mt-0 sm:space-x-6">
+                                    <div className="mt-2 flex items-center text-sm text-gray-500">
+                                        <svg className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                            <path
+                                                fillRule="evenodd"
+                                                d="M10 2.5a7.5 7.5 0 110 15 7.5 7.5 0 010-15zm0 1.5a6 6 0 100 12 6 6 0 000-12zM7.5 10a2.5 2.5 0 115 0 2.5 2.5 0 01-5 0z"
+                                                clipRule="evenodd"
+                                            />
+                                        </svg>
+                                        <span>26 years old</span>
+                                    </div>
+                                    <div className="mt-2 flex items-center text-sm text-gray-500">
+                                        <svg className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                            <path
+                                                fillRule="evenodd"
+                                                d="M10 2.5a7.5 7.5 0 110 15 7.5 7.5 0 010-15zm0 1.5a6 6 0 100 12 6 6 0 000-12zM7.5 10a2.5 2.5 0 115 0 2.5 2.5 0 01-5 0z"
+                                                clipRule="evenodd"
+                                            />
+                                        </svg>
+                                        <span>Male</span>
+                                    </div>
+                                    <div className="mt-2 flex items-center text-sm text-gray-500">
+                                        <svg className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                            <path
+                                                fillRule="evenodd"
+                                                d="M2 5a2 2 0 012-2h12a2 2 0 012 2v3h2a2 2 0 012 2v5a2 2 0 01-2 2h-2v3a2 2 0 01-2 2H4a2 2 0 01-2-2v-3H0a2 2 0 01-2-2V7a2 2 0 012-2zm2 2v3h12V7H4zm2 4a2 2 0 100 4 2 2 0 000-4zm8 0a2 2 0 100 4 2 2 0 000-4z"
+                                                clipRule="evenodd"
+                                            />
+                                        </svg>
+                                        <span>Govt ID: 123456789</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="mt-5">
+                            <h3 className="text-lg leading-6 font-medium text-gray-900">
+                                About
+                            </h3>
+                            <div className="mt-2 max-w-xl text-sm text-gray-500">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+                                    mauris ex, tristique ut libero vel, molestie interdum tellus.
+                                    Aenean consequat metus quis orci auctor, nec porttitor sapien
+                                    blandit. Suspendisse potenti.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="mt-5">
+                            <h3 className="text-lg leading-6 font-medium text-gray-900">
+                                Qualification
+                            </h3>
+                            <div className="mt-2 max-w-xl text-sm text-gray-500">
+                                <p>Bachelor's Degree in Computer Science</p>
+                            </div>
+                        </div>
+                        <div className="mt-5">
+                            <h3 className="text-lg leading-6 font-medium text-gray-900">
+                                Fields of Interest
+                            </h3>
+                            <div className="mt-2 max-w-xl text-sm text-gray-500">
+                                <p>Web Development, Artificial Intelligence, Cybersecurity</p>
+                            </div>
+                        </div>
+                        <div className="mt-5">
+                            <h3 className="text-lg leading-6 font-medium text-gray-900">
+                                Experience
+                            </h3>
+                            <div className="mt-2 max-w-xl text-sm text-gray-500">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+                                    mauris ex, tristique ut libero vel, molestie interdum tellus.
+                                    Aenean consequat metus quis orci auctor, nec porttitor sapien
+                                    blandit. Suspendisse potenti.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="mt-5">
+                            <h3 className="text-lg leading-6 font-medium text-gray-900">
+                                Recent Work Samples
+                            </h3>
+                            <div className="mt-2 max-w-xl text-sm text-gray-500">
+                                <ul className="border border-gray-200 rounded-md divide-y divide-gray-200">
+                                    <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                                        <div className="w-0 flex-1 flex">
+                                            <a
+                                                href="#"
+                                                className="truncate hover:text-gray-900"
+                                            >
+                                                Project 1
+                                            </a>
+                                        </div>
+                                        <div className="ml-4 flex-shrink-0">
+                                            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                                View
+                                            </a>
+                                        </div>
+                                    </li>
+                                    <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                                        <div className="w-0 flex-1 flex">
+                                            <a
+                                                href="#"
+                                                className="truncate hover:text-gray-900"
+                                            >
+                                                Project 2
+                                            </a>
+                                        </div>
+                                        <div className="ml-4 flex-shrink-0">
+                                            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                                View
+                                            </a>
+                                        </div>
+                                    </li>
+                                    <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                                        <div className="w-0 flex-1 flex">
+                                            <a
+                                                href="#"
+                                                className="truncate hover:text-gray-900"
+                                            >
+                                                Project 3
+                                            </a>
+                                        </div>
+                                        <div className="ml-4 flex-shrink-0">
+                                            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                                View
+                                            </a>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
+}
+
+export default ProfilePage;
+
+
+
+
---
+++ netapp/src/pages/SignIn.jsx
@@ -0,0 +1,140 @@
+import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function SignIn() {
+  const formArray = [1, 2, 3];
+  const [formNo, setFormNo] = useState(formArray[0])
+  const [state, setState] = useState({
+    name: '',
+    age: '',
+    gender: '',
+    govtId: '',
+    qualification: '',
+    about: '',
+    interest: '',
+    // expertise: '',
+    experience: '',
+    // preferredLanguageOfCmmunication: '',
+    workSamples: '',
+  })
+  const inputHandle = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value
+    })
+  }
+  const next = () => {
+    if (formNo === 1 && state.name && state.age && state.gender) {
+      if (state.age < 18) {
+        toast.error('Only Valid for users greater than or equal to 18 years of age')
+      }
+      else {
+        setFormNo(formNo + 1)
+      }
+    }
+    else if (formNo === 2 && state.govtId && state.qualification && state.about && state.interest && state.experience) {
+      setFormNo(formNo + 1)
+    } else {
+      toast.error('Please fillup all input field')
+    }
+  }
+  const pre = () => {
+    setFormNo(formNo - 1)
+  }
+  const finalSubmit = () => {
+    if (state.workSamples) {
+      toast.success('form submit success')
+    } else {
+      toast.error('Please fillup all input field')
+    }
+  }
+  return (
+    <div className="w-screen h-screen bg-slate-300 flex justify-center items-center">
+      <ToastContainer />
+      <div className="card w-[370px] rounded-md shadow-md bg-white p-5">
+        <div className='flex justify-center items-center'>
+          {
+            formArray.map((v, i) => <><div className={`w-[35px] my-3 text-white  ${formNo - 1 === i || formNo - 1 === i + 1 || formNo === formArray.length ? 'bg-blue-500' : 'bg-slate-400'} h-[35px] flex justify-center items-center`}>
+              {v}
+            </div>
+              {
+                i !== formArray.length - 1 && <div className={`w-[85px] h-[2px] ${formNo === i + 2 || formNo === formArray.length ? 'bg-blue-500' : 'bg-slate-400'}`}></div>
+              }
+            </>)
+          }
+        </div>
+        {
+          formNo === 1 && <div>
+            <div className='flex flex-col mb-2 '>
+              <label htmlFor="name">Name</label>
+              <input value={state.name} onChange={inputHandle} className='p-2 border border-slate-400 mt-1 outline-0 focus:border-blue-500 rounded-md' type="text" name='name' placeholder='name' id='name' />
+            </div>
+            <div className='flex flex-col mb-2'>
+              <label htmlFor="age">Age</label>
+              <input value={state.age} onChange={inputHandle} className='p-2 border border-slate-400 mt-1 outline-0 focus:border-blue-500 rounded-md' type="number" name='age' placeholder='age' id='age' min='18' />
+            </div>
+            <div className='flex flex-col mb-2'>
+              <label htmlFor="gender">Gender</label>
+              <select value={state.gender} onChange={inputHandle} className='p-2 border border-slate-400 mt-1 outline-0 focus:border-blue-500 rounded-md' name='gender' >
+                <option value="">Select gender</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="female">LGBTQ+</option>
+                <option value="female">Others</option>
+              </select>
+            </div>
+            <div className='mt-4 flex justify-center items-center'>
+              <button onClick={next} className='px-3 py-2 text-lg rounded-md w-full text-white bg-blue-500'>Next</button>
+            </div>
+          </div>
+        }
+
+        {
+          formNo === 2 && <div>
+            <div className='flex flex-col mb-2'>
+              <label className='text-slate-500' htmlFor="govtid">Goverment ID</label>
+              <input value={state.govtId} onChange={inputHandle} className='p-2 border border-slate-400 mt-1 outline-0 text-slate-500 focus:border-blue-500 rounded-md' type="file" name='govtId' placeholder='govt id' id='govt id' />
+            </div>
+            <div className='flex flex-col mb-2'>
+              <label className='text-slate-500' htmlFor="qualification">Highest Qualification</label>
+              <input value={state.qualification} onChange={inputHandle} className='p-2 border border-slate-400 mt-1 outline-0 text-slate-500 focus:border-blue-500 rounded-md' type="text" name='qualification' placeholder='qualification' id='qualification' />
+            </div>
+            <div className='flex flex-col mb-2'>
+              <label className='text-slate-500' htmlFor="address">About</label>
+              <textarea value={state.about} onChange={inputHandle} row='10' className='p-2 border border-slate-400 mt-1 outline-0 text-slate-500 focus:border-blue-500 rounded-md' type="text" name='about' placeholder='Give your profile an amazing bio...' ></textarea>
+            </div>
+            <div className='flex flex-col mb-2'>
+              <label className='text-slate-500' htmlFor="interest">Fields Of Interest</label>
+              <input value={state.interest} onChange={inputHandle} className='p-2 border border-slate-400 mt-1 outline-0 text-slate-500 focus:border-blue-500 rounded-md' type="text" name='interest' placeholder='interest' id='interest' />
+            </div>
+            <div className='flex flex-col mb-2'>
+              <label className='text-slate-500' htmlFor="qualification">Experience</label>
+              <input value={state.experience} onChange={inputHandle} className='p-2 border border-slate-400 mt-1 outline-0 text-slate-500 focus:border-blue-500 rounded-md' type="text" name='experience' placeholder='experience' id='experience' />
+            </div>
+            <div className='mt-4 gap-3 flex justify-center items-center'>
+              <button onClick={pre} className='px-3 py-2 text-lg rounded-md w-full text-white bg-blue-500'>Previous</button>
+              <button onClick={next} className='px-3 py-2 text-lg rounded-md w-full text-white bg-blue-500'>Next</button>
+            </div>
+          </div>
+        }
+
+        {
+          formNo === 3 && <div>
+            <div className='flex flex-col mb-2'>
+              <label htmlFor="workSamples">Add Recent Work Samples (Max:3)</label>
+              <input value={state.workSamples} onChange={inputHandle} className='p-2 border border-slate-400 mt-1 outline-0 focus:border-blue-500 rounded-md' type="file" name='workSamples' id='workSamples' accept='.pdf' multiple />
+            </div>
+            <div className='mt-4 gap-3 flex justify-center items-center'>
+              <button onClick={pre} className='px-3 py-2 text-lg rounded-md w-full text-white bg-blue-500'>Previous</button>
+              <button onClick={finalSubmit} className='px-3 py-2 text-lg rounded-md w-full text-white bg-blue-500'>Submit</button>
+            </div>
+          </div>
+        }
+
+      </div>
+    </div>
+  );
+}
+
+export default SignIn;

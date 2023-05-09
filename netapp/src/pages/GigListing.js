e2c037d38ee1038c7a74e9534d663128453c98d2
Akanksha <akanksha012btit21@igdtuw.ac.in>
2023-05-09 18:06:03 +0530

    changes

--- netapp/src/pages/GigListing.js
+++ netapp/src/pages/GigListing.js
@@ -125,7 +125,7 @@ function GigListing() {
                 </div>
 
                 <div class="sm:col-span-3">
-                  <label for="age" class="block text-sm font-medium leading-6 text-gray-900">Fresher Specific?</label>
+                  <label for="age" class="block text-sm font-medium leading-6 text-gray-900">Fresher Specific???</label>
                   <div class="mt-2">
                     <select id="fresherspecific" name="fresherspecific" autocomplete="fresherspecific" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6" onChange={handleInput} >
                     <option value="">--Select--</option>

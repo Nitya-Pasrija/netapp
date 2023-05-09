39c1edbb59f0d2ae3d1eebae41aae38460d9e72b
Akanksha <akanksha012btit21@igdtuw.ac.in>
2023-05-09 17:35:06 +0530

    backend retreival

--- netapp/src/pages/GigListing.js
+++ netapp/src/pages/GigListing.js
@@ -12,7 +12,7 @@ function GigListing() {
     deadline: "",
     remuneration: "",
     fresherSpecific: "",
-  });
+  }); 
 
   // Handle Inputs
   const handleInput = (event) => {
@@ -127,9 +127,10 @@ function GigListing() {
                 <div class="sm:col-span-3">
                   <label for="age" class="block text-sm font-medium leading-6 text-gray-900">Fresher Specific?</label>
                   <div class="mt-2">
-                    <select id="fresherspecific" name="fresherspecific" autocomplete="fresherspecific" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6" value={user.fresherSpecific} onChange={handleInput} >
-                      <option>Yes</option>
-                      <option>No</option>
+                    <select id="fresherspecific" name="fresherspecific" autocomplete="fresherspecific" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6" onChange={handleInput} >
+                    <option value="">--Select--</option>
+                      <option value={"Yes"}>Yes</option>
+                      <option value={"No"}>No</option>
                     </select>
                   </div>
                 </div>
--- netapp/src/pages/Gigs.js
+++ netapp/src/pages/Gigs.js
@@ -1,80 +1,76 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Naavbaar from '../components/Navbaar'
 import CarouselGigs from '../components/CarouselGigs'
-function Gigs () {
+
+function GigCard({ gig }) {
+  const [applied, setApplied] = useState(false);
+
+  const handleClick = () => {
+    setApplied(true);
+  };
+
   return (
-    <div class=""> 
-        <Naavbaar/>
-        <CarouselGigs/>
-        <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-12 my-5">
-        <div class="-m-1 flex flex-wrap md:-m-2">
-            <div class="flex w-1/3 flex-wrap">
-            <div class="w-full p-1 md:p-2">
-                <img
-                alt="gallery" 
-                class="block h-full w-full rounded-lg object-cover object-center"
-                src="https://images.unsplash.com/photo-1634245482394-1bcf5ccffcc0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjh8fGdyYXBoaWMlMjBkZXNpZ25lcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
-            </div>
-            </div>
-            <div class="flex w-1/3 flex-wrap">
-            <div class="w-full p-1 md:p-2">
-                <img
-                alt="gallery"
-                class="block h-full w-full rounded-lg object-cover object-center"
-                src="https://images.unsplash.com/photo-1532800783378-1bed60adaf58?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTB8fHZvaWNlJTIwYWN0b3J8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" />
-            </div>
-            </div>
-            <div class="flex w-1/3 flex-wrap">
-            <div class="w-full p-1 md:p-2">
-                <img
-                alt="gallery"
-                class="block h-full w-full rounded-lg object-cover object-center"
-                src="https://images.unsplash.com/photo-1616499370260-485b3e5ed653?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZnJlZWxhbmNpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" />
-            </div>
-            </div>
-            <div class="flex w-1/3 flex-wrap">
-            <div class="w-full p-1 md:p-2">
-                <img
-                alt="gallery"
-                class="block h-full w-full rounded-lg object-cover object-center"
-                src="https://images.unsplash.com/photo-1557838923-2985c318be48?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGlnaXRhbCUyMG1hcmtldGluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
-            </div>
-            </div>
-            <div class="flex w-1/3 flex-wrap">
-            <div class="w-full p-1 md:p-2">
-                <img
-                alt="gallery"
-                class="block h-full w-full rounded-lg object-cover object-center"
-                src="https://images.unsplash.com/photo-1542038784456-1ea8e935640e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG9ncmFwaHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" />
-            </div>
-            </div>
-            <div class="flex w-1/3 flex-wrap">
-            <div class="w-full p-1 md:p-2">
-                <img
-                alt="gallery"
-                class="block h-full w-full rounded-lg object-cover object-center"
-                src="https://images.unsplash.com/photo-1535432715554-75d3f522ab18?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGFwcCUyMGRldmVsb3BtZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" />
-            </div>
-            </div>
-            <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-        <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
-    </a>
-    <div class="p-5">
-        <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-        </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Read more
-            <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-        </a>
-    </div>
-</div>
+    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden">
+      <img
+        className="h-56 w-full object-cover"
+        src="https://media.istockphoto.com/id/1371339413/photo/co-working-team-meeting-concept-businessman-using-smart-phone-and-digital-tablet-and-laptop.jpg?b=1&s=170667a&w=0&k=20&c=GnW36mPEYr3TRpPZNLtT75u8v2HOczvRxGckWUN3hVg="
+        alt="Gig image"
+      />
+      <div className="p-6">
+        <h2 className="font-bold text-xl mb-2">Gig Name: {gig.gigName}</h2>
+        <p className="text-gray-500 text-base mb-2">Category: {gig.category}</p>
+        <p className="text-gray-700 text-base mb-4">Work description: {gig.workDesc}</p>
+        <p className="text-gray-700 text-base mb-4">Remuneration: {gig.remuneration}</p>
+        <p className="text-gray-700 text-base mb-4">Deadline: {gig.deadline}</p>
+        <p className="text-gray-700 text-base mb-4">Fresherspecific: {gig.fresherSpecific}</p>
+        <div className="flex justify-end">
+          {!applied ? (
+            <button
+              type="button"
+              onClick={handleClick}
+              className="bg-blue-600 text-blue-100 rounded-lg py-2 px-4 m-2 shadow hover:shadow-xl hover:bg-blue-700 duration-300"
+            >
+              Apply
+            </button>
+          ) : (
+            <button
+              type="button"
+              className="bg-green-600 text-green-100 rounded-lg py-2 px-4 m-2 shadow hover:shadow-xl hover:bg-green-700 duration-300"
+            >
+              Applied
+            </button>
+          )}
         </div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Gigs
+function Gigs() {
+  const [gigs, setGigs] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('/gigs');
+      const data = await response.json();
+      setGigs(data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="">
+      <Naavbaar />
+      <CarouselGigs />
+      <div className="container mx-auto px-5 py-2 lg:px-32 lg:pt-12 my-5">
+        <div className="-m-1 flex flex-wrap md:-m-2">
+          {gigs.map((gig) => (
+            <GigCard key={gig.id} gig={gig} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Gigs;
